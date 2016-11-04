d2d761012ca68fcf7148d3eca012e9f91ee93561
Ursegorus <ursegorus@gmail.com>
2016-11-04 20:05:26 +0300

    Initial commit

---
+++ future-vendor-blobs.mk
@@ -0,0 +1,18 @@
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/etc,system/etc)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/future/proprietary/vendor/lib64,system/vendor/lib64)
+
